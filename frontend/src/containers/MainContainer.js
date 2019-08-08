cbd0cf4b078d8375d626d74e7bef9936d7b374f6
Arledis <arleidysamaya_18@hotmail.com>
2019-08-08 17:00:44 +0100

    initial setup

---
+++ frontend/src/containers/MainContainer.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+
+
+class MainContainer extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+  render(){
+    return (
+      <div>
+      <p>MainContainer</p>
+      </div>
+    )
+  }
+}
+
+export default MainContainer;

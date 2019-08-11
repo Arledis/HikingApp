1041a2ea8df81f24083c8f5459fe6972f5851d80
dafyddllyr <dafyddllyr@gmail.com>
2019-08-11 13:25:31 +0100

    Feature basic implementation

--- frontend/src/components/user/RouteCreator.js
+++ frontend/src/components/user/RouteCreator.js
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './RouteCreator.css'
 import './SideBar.css'
 
-const RouteCreator = ({newRoute, currentCoords}) => {
+class RouteCreator extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      start: null,
+      end: null
+    }
+    this.handleSaveRoute = this.handleSaveRoute.bind(this)
+  }
 
-return(
-  <div className="sidebar-component">
-    <h2>Start: {currentCoords}</h2>
-    <h2>End: {newRoute.end}</h2>
-    <hr />
-  </div>
-)
+  handleSaveRoute() {
+console.log("saving");
+  }
+
+  render() {
+    return(
+      <div className="sidebar-component">
+      <h2>Start: {this.props.currentCoords}</h2>
+      <h2>End: </h2>
+      <hr />
+      <h2>CHART HERE</h2>
+      <hr />
+      <button onClick={this.handleSaveRoute}>Save Route</button>
+      </div>
+    )
+  }
+
 
 }
 

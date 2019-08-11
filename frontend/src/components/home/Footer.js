e686394c9c6dd66aef2a96202faf9a104a34c0ad
StewartMacLeman <stewart_macleman@hotmail.com>
2019-08-11 15:20:14 +0100

    Footer and InfoBox with CSS files added, draft versions

---
+++ frontend/src/components/home/Footer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Footer.css'
+const Footer = () => {
+
+  return(
+
+    <div className="footer">
+
+    <div id="footer-address">
+    <h1>Contact Information:<h1>
+    <p>Address: DACS.Inc. <br />Code Towers, 123 Made Up Street, Glasgow<p>
+    <p>Email: Dacs@Hotpost.org</p>
+    </div>
+
+    <div id="footer-back-to-top">
+    <h1>Back to the top! (tbc)</h1>
+    </div>
+
+    <div id="footer-snt-link">
+    <h1>Scottish National Trail Link (tbc)</h1>
+    </div>
+
+    </div>
+  )
+
+}
+
+export default Footer;
---
+++ frontend/src/components/home/InfoBox.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './InfoBox.css'
+const InfoBox = () => {
+
+  return(
+
+    <div id="infobox">
+
+    <div>
+    <h1>Left-Hand Box</h1>
+    <p>Something very interesting!</p>
+    </div>
+
+    <div>
+    <h1>Centre Box</h1>
+    <p>Something very very interesting!</p>
+    </div>
+
+    <div>
+    <h1>Right-Hand Box</h1>
+    <p>Something not so interesting, but important!</p>
+    </div>
+
+    </div>
+  )
+}

21f67ae634cbf1fae9f4b97fa20dabe2e0346a59
craiggough376 <craig_gough_376@hotmail.com>
2019-08-12 12:00:48 +0100

    homepage added

--- frontend/src/components/home/InfoBox.js
+++ frontend/src/components/home/InfoBox.js
@@ -1,26 +1,21 @@
 import React from 'react';
 import './InfoBox.css'
-const InfoBox = () => {
+
+
+const InfoBox = ({title, blurb, pic}) => {
 
   return(
 
     <div id="infobox">
 
-    <div>
-    <h1>Left-Hand Box</h1>
-    <p>Something very interesting!</p>
-    </div>
-
-    <div>
-    <h1>Centre Box</h1>
-    <p>Something very very interesting!</p>
-    </div>
-
-    <div>
-    <h1>Right-Hand Box</h1>
-    <p>Something not so interesting, but important!</p>
-    </div>
+      <div class="info-box">
+        <img src={pic} alt="pic"/>
+        <h1>{title}</h1>
+        <p>{blurb}</p>
+      </div>
 
     </div>
   )
 }
+
+export default InfoBox;
